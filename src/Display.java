1f13171724cfcb718b645e6f01151a86266c386e
rics <rih.dze@gmail.com>
2021-09-10 16:55:22 +0300

    Initial commit

---
+++ src/Display.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Display extends JFrame {
+
+    private Canvas canvas;
+    public Display(int width, int height, Input input){
+        setTitle("My Awesome RPG Game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setFocusable(false);
+        add(canvas);
+        addKeyListener(input);
+        pack();
+
+        canvas.createBufferStrategy(3);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
+    public void render(Game game){
+        BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0,canvas.getWidth(), canvas.getHeight());
+
+        game.getGameObjects().forEach(gameObject -> graphics.drawImage(gameObject.getSprite(),
+                gameObject.getPosition().getX(), gameObject.getPosition().getY(), null));
+
+
+
+
+        graphics.dispose();
+        bufferStrategy.show();
+
+    }
+
+}
---
+++ src/Launcher.java
@@ -0,0 +1,11 @@
+public class Launcher {
+
+
+    public static void main(String[] args) {
+//        Display display = new Display(800, 600  );
+        new Thread(new GameLoop(new Game(800,600))).start();
+    }
+
+
+
+}
